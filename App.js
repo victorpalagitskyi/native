bda1f998fff304262bf35dbdfaf613f32c83dec5
Victor20012029103 <112655698+Victor20012029103@users.noreply.github.com>
2023-07-16 18:13:12 +0300

    second

--- App.js
+++ App.js
@@ -1,12 +1,14 @@
-import { StatusBar } from 'expo-status-bar';
+
 import { StyleSheet, Text, View } from 'react-native';
+import LoginScreen from './Screens/LoginScreen';
+import RegistrationScreen from './Screens/RegistrationScreen';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+      <LoginScreen></LoginScreen>
+       <RegistrationScreen></RegistrationScreen>
+   </View>
   );
 }
 
---
+++ Screens/LoginScreen.jsx
@@ -0,0 +1,95 @@
+import { useState } from "react";
+import {
+  Button,
+  TextInput,
+  Text,
+  View,
+  StyleSheet,
+  Alert,
+  Keyboard,
+  TouchableWithoutFeedback,
+  KeyboardAvoidingView,
+  Platform,
+} from "react-native";
+
+const initialState = {
+  email: "",
+  password: "",
+};
+const LoginScreen = () => {
+  const [state, setState] = useState(initialState);
+  const [isHidden, setIsHidden] = useState(true);
+  const [focusedField, setFocusedField] = useState("");
+
+  // const handleOutsideClick = () => {
+  //    Keyboard.dismiss
+  // }
+
+  const onLogin = () => {
+    Alert.alert("Credentials", `${state.email} + ${state.password}`);
+  };
+
+  return (
+    <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+      <View style={styles.container}>
+        <KeyboardAvoidingView
+          behavior={Platform.OS == "ios" ? "padding" : "height"}
+        >
+          <TextInput
+            style={styles.input}
+            placeholder="Email adress"
+            onChangeText={(text) =>
+              setState((prev) => ({ ...prev, email: text }))
+            }
+            value={state.email}
+          />
+          <TextInput
+            style={styles.input}
+            placeholder="Password"
+            secureTextEntry={isHidden ? true : false}
+            onChangeText={(text) =>
+              setState((prev) => ({ ...prev, password: text }))
+            }
+            value={state.password}
+          ></TextInput>
+          <Button
+            title={"Sign In"}
+            style={styles.title}
+            onPress={onLogin}
+          ></Button>
+        </KeyboardAvoidingView>
+      </View>
+    </TouchableWithoutFeedback>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+    backgroundColor: "#ecf0f1",
+    padding: 12,
+  },
+  title: {
+    marginTop: 20,
+    paddingVertical: 8,
+    borderWidth: 4,
+    borderColor: "#20232a",
+    borderRadius: 6,
+    backgroundColor: "#61dafb",
+    color: "#20232a",
+    textAlign: "center",
+    fontSize: 20,
+    fontWeight: "bold",
+  },
+  input: {
+    width: 200,
+    height: 44,
+    padding: 10,
+    borderWidth: 1,
+    borderColor: "black",
+    marginBottom: 10,
+  },
+});
+export default LoginScreen;
